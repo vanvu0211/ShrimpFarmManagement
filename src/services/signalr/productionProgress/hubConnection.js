2ce5747272a603a121a18d982338829dad5baa5a
vanvu0211 <vu34304@gmail.com>
2025-03-31 14:32:28 +0700

    error code

--- src/services/signalr/productionProgress/hubConnection.js
+++ src/services/signalr/productionProgress/hubConnection.js
@@ -1,7 +1,7 @@
 import { HubConnectionBuilder, HttpTransportType } from '@microsoft/signalr';
 
-const connection = new HubConnectionBuilder()
-    .withUrl('http://103.170.122.142:5000/machineHub', {
+const connection = new HubConnectionBuilder() //Lớp tạo một kết nối tới một SignalR Hub
+    .withUrl('https://103.170.122.142:5000/machineHub', {
         transport: HttpTransportType.WebSockets,
         skipNegotiation: true,
     })
